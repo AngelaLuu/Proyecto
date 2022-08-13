20baeb36a96091ff3990299cd5fc8173f9e7a990
AngelaLuu <angelaht2601@gmail.com>
2022-08-13 12:26:48 -0500

    se agrega el Proyecto

---
+++ conexion.php
@@ -0,0 +1,14 @@
+<?php 
+
+$conexion= mysqli_connect("localhost:3307", "root", "", "Agendamiento_Proyecto");
+
+/*/
+if($conexion) {
+   echo 'Conectado exitosamente a la base de datos';
+}
+else {
+   echo 'No fue posible conectar a la base de datos';
+}
+*/
+
+?> 
